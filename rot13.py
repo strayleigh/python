541053d123c3aec0a6edeccecd989d28e9f99aa6
strayleigh <122590351+strayleigh@users.noreply.github.com>
2024-11-10 10:08:42 +0700

    Delete rot13.py

--- rot13.py
+++
@@ -1,8 +0,0 @@
-import codecs
-
-text = 'cvpbPGS{arkg_gvzr_V\'yy_gel_2_ebhaqf_bs_ebg13_uJdSftmh}'
-rot13 = codecs.encode(text, 'rot_13')
-print(rot13)
-
-text = codecs.decode(rot13, 'rot_13')
-print(text)
